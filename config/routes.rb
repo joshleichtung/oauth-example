e4210d55f9780615eb33c2c4943b2feb2585d139
Josh Leichtung <joshleichtung@gmail.com>
2017-07-26 14:16:48 -0700

    Add gh oauth

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "dogs#index"
   devise_for :users
   resources :dogs
+  get '/auth/:github/callback', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
