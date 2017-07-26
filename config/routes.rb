48fc3c824060df064a364460f4c7bf20187c5c6b
Josh Leichtung <joshleichtung@gmail.com>
2017-07-26 10:51:45 -0700

    Add dog scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root "application#show"
+  resources :dogs
+  root "dogs#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
