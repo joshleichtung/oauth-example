b60ff4dd2cb862a925e2faf0530d80df0af015e5
Josh Leichtung <joshleichtung@gmail.com>
2017-07-26 11:15:13 -0700

    Add basic devise login functionality

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,5 +1,6 @@
 class DogsController < ApplicationController
   before_action :set_dog, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
 
   # GET /dogs
   # GET /dogs.json
