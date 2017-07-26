2102df0ad4af2df314275168ba12b2fc11aff510
Josh Leichtung <joshleichtung@gmail.com>
2017-07-26 11:12:56 -0700

    Add seeds and flash to layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Dog.create(name: "Mascis", age: 3, description: "Bearded. Crazy")
+Dog.create(name: "Whiskey", age: 3, description: "Like a piggy")
+Dog.create(name: "Charlie", age: 5, description: "Strong. Handsome.")
+Dog.create(name: "Lilo", age: 9, description: "Needy. Teeth like chiclets")
